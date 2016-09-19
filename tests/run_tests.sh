755bc550dea79145230eec7bda000be07b2e9e92
Chris Jefferson <caj21@st-andrews.ac.uk>
2016-09-19 21:20:59 +0100

    Add --tryharder, improved error messages

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,4 +7,9 @@ for i in $(ls -d tests/*/); do
     diff $i/verbose-output.txt <( cd $i && ./go.sh "$CHECKPROG" --verbose)
     diff $i/output.html <(cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && cat /tmp/html.tmp && rm /tmp/html.tmp)
     diff $i/output.json <(cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && cat /tmp/json.tmp && rm /tmp/json.tmp)
+    rm -f /tmp/try-harder.html /tmp/try-harder.json /tmp/try-harder.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json) > /tmp/try-harder.txt
+    diff /tmp/try-harder.html $i/try-harder.html
+    diff /tmp/try-harder.json $i/try-harder.json
+    diff /tmp/try-harder.txt $i/try-harder.txt
 done
---
+++ tests/tests/diffcheck/testdir/stderrstdout/progcat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "line 1"
+>&2 echo "line 2"
+echo "line 3"
+
