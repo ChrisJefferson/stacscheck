1105a8f124571575f38af2c8e4bcc00954b51e1a
Chris Jefferson <caj21@st-andrews.ac.uk>
2016-09-19 13:34:07 +0100

    Improve error messages

--- running-all-students/all-students.sh
+++ running-all-students/all-students.sh
@@ -22,6 +22,7 @@ practical=$(get_abs_filename "$1")
 submissions=$(get_abs_filename "$2")
 outdir=$(get_abs_filename "$3")
 
+counter=0
 
 for dir in "${submissions}/"*/; do
     (
@@ -34,5 +35,10 @@ for dir in "${submissions}/"*/; do
                    --html="${outdir}/html/${studentid}.html" \
                    --json="${outdir}/json/${studentid}.json" \
                      > "${outdir}/txt/${studentid}"
-    )
+    ) &
+    counter=$((counter+1))
+    if (( counter > 3 )); then
+        counter=0
+        wait
+    fi;
 done
