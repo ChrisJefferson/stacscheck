9eb6c8d15cb768a3176155161577c4bebdab6322
Chris Jefferson <caj21@st-andrews.ac.uk>
2017-02-21 22:09:51 +0000

    Add .travis.yml

--- tests/fix_tests.sh
+++ tests/fix_tests.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+CURRENTDIR=$(pwd)
+FILT='s|'$CURRENTDIR'|DIR|g'
+
+function filtfile {
+    sed ${FILT} $1
+}
+
 CHECKPROG=$(pwd)/../stacscheck
 for i in $(ls -d tests/*/); do
     echo Testing $i
-    ( cd $i && ./go.sh "$CHECKPROG") > $i/output.txt
-    ( cd $i && ./go.sh "$CHECKPROG" --verbose ) > $i/verbose-output.txt
-    ( cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && cat /tmp/html.tmp && rm /tmp/html.tmp) > $i/output.html
-    ( cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && cat /tmp/json.tmp && rm /tmp/json.tmp) > $i/output.json
+    ( cd $i && ./go.sh "$CHECKPROG" | sed ${FILT} ) > $i/output.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --verbose | sed ${FILT} ) > $i/verbose-output.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && sed ${FILT} /tmp/html.tmp && rm /tmp/html.tmp) > $i/output.html
+    ( cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && sed ${FILT} /tmp/json.tmp && rm /tmp/json.tmp) > $i/output.json
     rm -f /tmp/try-harder.html /tmp/try-harder.json /tmp/try-harder.txt
-    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json) > /tmp/try-harder.txt
-    cat /tmp/try-harder.html > $i/try-harder.html
-    cat /tmp/try-harder.json > $i/try-harder.json
-    cat /tmp/try-harder.txt  > $i/try-harder.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json | sed ${FILT}) > /tmp/try-harder.txt
+    sed ${FILT} /tmp/try-harder.html > $i/try-harder.html
+    sed ${FILT} /tmp/try-harder.json > $i/try-harder.json
+    sed ${FILT} /tmp/try-harder.txt  > $i/try-harder.txt
 done
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+mkdir tests/checkout-baddir/testdir/abc
+chmod -rwx tests/checkout-baddir/testdir/abc
+set -o errexit 
+
+CURRENTDIR=$(pwd)
+FILT='s|'$CURRENTDIR'|DIR|g'
+
+function filtfile {
+    sed ${FILT} $1
+}
+
+echo Current dir is $CURRENTDIR
 CHECKPROG=$(pwd)/../stacscheck
 for i in $(ls -d tests/*/); do
     echo Testing $i
-    diff $i/output.txt <( cd $i && ./go.sh "$CHECKPROG")
-    diff $i/verbose-output.txt <( cd $i && ./go.sh "$CHECKPROG" --verbose)
-    diff $i/output.html <(cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && cat /tmp/html.tmp && rm /tmp/html.tmp)
-    diff $i/output.json <(cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && cat /tmp/json.tmp && rm /tmp/json.tmp)
+    diff $i/output.txt  <( cd $i && ./go.sh "$CHECKPROG" | sed ${FILT})
+    diff $i/verbose-output.txt <( cd $i && ./go.sh "$CHECKPROG" --verbose | sed ${FILT})
+    diff $i/output.html <(cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && sed ${FILT} /tmp/html.tmp && rm /tmp/html.tmp)
+    diff $i/output.json <(cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && sed ${FILT} /tmp/json.tmp && rm /tmp/json.tmp)
     rm -f /tmp/try-harder.html /tmp/try-harder.json /tmp/try-harder.txt
-    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json) > /tmp/try-harder.txt
-    diff /tmp/try-harder.html $i/try-harder.html
-    diff /tmp/try-harder.json $i/try-harder.json
-    diff /tmp/try-harder.txt $i/try-harder.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json || true) > /tmp/try-harder.txt
+    if [ -e /tmp/try-harder.html ]; then
+        diff $i/try-harder.html <(filtfile /tmp/try-harder.html)
+    fi;
+    if [ -e /tmp/try-harder.json ]; then
+        diff $i/try-harder.json <(filtfile /tmp/try-harder.json)
+    fi;
+    if [ -e /tmp/try-harder.txt ]; then
+        diff $i/try-harder.txt <(filtfile /tmp/try-harder.txt)
+    fi;
 done
