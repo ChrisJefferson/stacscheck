ce247745e7fe8688a2a6221609f08e5cf2c26076
Chris Jefferson <caj21@st-andrews.ac.uk>
2018-10-30 14:59:41 +0000

    Remove explicit python2/3 testing

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -18,36 +18,33 @@ function filtfile {
     sed ${FILT} $1 | sed ${FILT2}
 }
 
-for python in python2 python3; do
-    echo Testing $python
-    for i in $(ls -d tests/*/); do
-        echo Testing $i
-        diff $i/output.txt  <( cd $i && ./go.sh "$python $CHECKPROG" | sed ${FILT} | sed ${FILT2})
-        diff $i/verbose-output.txt <( cd $i && ./go.sh "$python $CHECKPROG" --verbose | sed ${FILT} | sed ${FILT2})
-        diff $i/output.html <(cd $i && ./go.sh "$python $CHECKPROG" --html /tmp/html.tmp > /dev/null && sed ${FILT} /tmp/html.tmp | sed ${FILT2} && rm /tmp/html.tmp)
-        diff $i/output.json <(cd $i && ./go.sh "$python $CHECKPROG" --json /tmp/json.tmp > /dev/null && sed ${FILT} /tmp/json.tmp | sed ${FILT2} && rm /tmp/json.tmp)
-        rm -f /tmp/try-harder.html /tmp/try-harder.json /tmp/try-harder.txt
-        ( cd $i && ./go.sh "$python $CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json || true) > /tmp/try-harder.txt
-        if [ -e /tmp/try-harder.html ]; then
-            diff $i/try-harder.html <(filtfile /tmp/try-harder.html)
-        fi;
-        if [ -e /tmp/try-harder.json ]; then
-            diff $i/try-harder.json <(filtfile /tmp/try-harder.json)
-        fi;
-        if [ -e /tmp/try-harder.txt ]; then
-            diff $i/try-harder.txt <(filtfile /tmp/try-harder.txt)
-        fi;
+for i in $(ls -d tests/*/); do
+    echo Testing $i
+    diff $i/output.txt  <( cd $i && ./go.sh "$CHECKPROG" | sed ${FILT} | sed ${FILT2})
+    diff $i/verbose-output.txt <( cd $i && ./go.sh "$CHECKPROG" --verbose | sed ${FILT} | sed ${FILT2})
+    diff $i/output.html <(cd $i && ./go.sh "$CHECKPROG" --html /tmp/html.tmp > /dev/null && sed ${FILT} /tmp/html.tmp | sed ${FILT2} && rm /tmp/html.tmp)
+    diff $i/output.json <(cd $i && ./go.sh "$CHECKPROG" --json /tmp/json.tmp > /dev/null && sed ${FILT} /tmp/json.tmp | sed ${FILT2} && rm /tmp/json.tmp)
+    rm -f /tmp/try-harder.html /tmp/try-harder.json /tmp/try-harder.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --tryharder --html /tmp/try-harder.html --json /tmp/try-harder.json || true) > /tmp/try-harder.txt
+    if [ -e /tmp/try-harder.html ]; then
+        diff $i/try-harder.html <(filtfile /tmp/try-harder.html)
+    fi;
+    if [ -e /tmp/try-harder.json ]; then
+        diff $i/try-harder.json <(filtfile /tmp/try-harder.json)
+    fi;
+    if [ -e /tmp/try-harder.txt ]; then
+        diff $i/try-harder.txt <(filtfile /tmp/try-harder.txt)
+    fi;
 
-        rm -f /tmp/verbose-test.html /tmp/verbose-test.json /tmp/verbose-test.txt
-        ( cd $i && ./go.sh "$python $CHECKPROG" --verbose --html /tmp/verbose-test.html --json /tmp/verbose-test.json || true) > /tmp/verbose-test.txt
-        if [ -e /tmp/verbose-test.html ]; then
-            diff $i/verbose-test.html <(filtfile /tmp/verbose-test.html)
-        fi;
-        if [ -e /tmp/try-harder.json ]; then
-            diff $i/verbose-test.json <(filtfile /tmp/verbose-test.json)
-        fi;
-        if [ -e /tmp/try-harder.txt ]; then
-            diff $i/verbose-test.txt <(filtfile /tmp/verbose-test.txt)
-        fi;
-    done
+    rm -f /tmp/verbose-test.html /tmp/verbose-test.json /tmp/verbose-test.txt
+    ( cd $i && ./go.sh "$CHECKPROG" --verbose --html /tmp/verbose-test.html --json /tmp/verbose-test.json || true) > /tmp/verbose-test.txt
+    if [ -e /tmp/verbose-test.html ]; then
+        diff $i/verbose-test.html <(filtfile /tmp/verbose-test.html)
+    fi;
+    if [ -e /tmp/try-harder.json ]; then
+        diff $i/verbose-test.json <(filtfile /tmp/verbose-test.json)
+    fi;
+    if [ -e /tmp/try-harder.txt ]; then
+        diff $i/verbose-test.txt <(filtfile /tmp/verbose-test.txt)
+    fi;
 done
